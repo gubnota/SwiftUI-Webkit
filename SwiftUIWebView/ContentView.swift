dd0ace13fd881ad751c2d9408a964af8c5cd66e2
马拉迪 <maladi@meihuasoft.com>
2020-06-08 09:38:43 +0800

    added a WebKit project

--- SwiftUIWebView/ContentView.swift
+++ SwiftUIWebView/ContentView.swift
@@ -7,10 +7,28 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+//    var body: some View {
+//        Text("Hello, World!")
+//    }
+
+//load an html file
+let testHTML = Bundle.main.url(forResource: "www/index", withExtension: "html")
+var body: some View {
+    WebView(request: URLRequest(url: testHTML!))
+}
+
+}
+
+struct WebView: UIViewRepresentable {
+    let request: URLRequest
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<WebView>) {
+         uiView.load(request)
     }
 }
 
